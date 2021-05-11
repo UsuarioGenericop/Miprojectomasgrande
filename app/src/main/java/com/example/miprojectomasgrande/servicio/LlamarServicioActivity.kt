688993940dc6c22812ba3177d1dffc89f0cdaa6f
UsuarioGenericop <andydquintero@gmail.com>
2021-05-10 19:16:17 -0500

    Se agrega el adaptador para lista de games

--- app/src/main/java/com/example/miprojectomasgrande/servicio/LlamarServicioActivity.kt
+++ app/src/main/java/com/example/miprojectomasgrande/servicio/LlamarServicioActivity.kt
@@ -2,6 +2,7 @@ package com.example.miprojectomasgrande.servicio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.miprojectomasgrande.R
 import com.example.miprojectomasgrande.servicio.data.ApiClient
 import com.example.miprojectomasgrande.servicio.data.DogamiGameResult
@@ -10,26 +11,34 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class LlamarServicioActivity : AppCompatActivity() {
+    lateinit var resultTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_llamar_servicio)
+        resultTextView = findViewById(R.id.textView)
         getGames()
     }
+
     private fun getGames() {
+        Thread(Runnable {
+            ApiClient.apiService.getGames().enqueue(object :
+                Callback<MutableList<DogamiGameResult>> {
+                override fun onFailure(call: Call<MutableList<DogamiGameResult>>, t: Throwable) {
+                    resultTextView.post {
+                        resultTextView.text = "Error loading data :("
+                    }
+                }
 
-        ApiClient.apiService.getGames().enqueue(object : Callback<MutableList<DogamiGameResult>> {
-            override fun onFailure(call: Call<MutableList<DogamiGameResult>>, t: Throwable) {
-                //Show error
-            }
-
-            override fun onResponse(
-                call: Call<MutableList<DogamiGameResult>>,
-                response: Response<MutableList<DogamiGameResult>>
-            ) {
-                val usersResponse = response.body()
-            }
-        })
-
+                override fun onResponse(
+                    call: Call<MutableList<DogamiGameResult>>,
+                    response: Response<MutableList<DogamiGameResult>>
+                ) {
+                    resultTextView.post {
+                        resultTextView.text = response.body().toString()
+                    }
+                }
+            })
+        }).start()
     }
 
 }
---
+++ app/src/main/java/com/example/miprojectomasgrande/servicio/adapter/GameAdapter.kt
@@ -0,0 +1,42 @@
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.miprojectomasgrande.R
+import com.example.miprojectomasgrande.servicio.data.DogamiGameResult
+
+
+class GameAdapter(val context: Context?, data: List<DogamiGameResult>) :
+    RecyclerView.Adapter<GameAdapter.ViewHolder>() {
+
+    private val itemsList: List<DogamiGameResult> = data
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View =
+            LayoutInflater.from(context).inflate(R.layout.view_game_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val game: DogamiGameResult = itemsList[position]
+        Glide.with(holder.gameImage)
+            .load(game.imageUrl)
+            .centerCrop()
+            .into(holder.gameImage)
+        holder.gameValue.text = game.name
+    }
+
+    override fun getItemCount(): Int {
+        return itemsList.size
+    }
+
+    class ViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        var gameImage: ImageView = itemView.findViewById(R.id.ivCereza)
+        var gameValue: TextView = itemView.findViewById(R.id.tvCereza)
+    }
+}
