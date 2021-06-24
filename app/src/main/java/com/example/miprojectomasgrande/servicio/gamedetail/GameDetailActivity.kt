506970478845f68eff2207481e9580e361015863
UsuarioGenericop <andydquintero@gmail.com>
2021-06-24 18:35:56 -0500

    Se configura los parametros para cada juego

--- app/src/main/java/com/example/miprojectomasgrande/servicio/gamedetail/GameDetailActivity.kt
+++ app/src/main/java/com/example/miprojectomasgrande/servicio/gamedetail/GameDetailActivity.kt
@@ -2,13 +2,22 @@ package com.example.miprojectomasgrande.servicio.gamedetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toolbar
+import com.bumptech.glide.Glide
 import com.example.miprojectomasgrande.R
 import com.example.miprojectomasgrande.servicio.data.DogamiGameResult
 
 class GameDetailActivity : AppCompatActivity() {
 
     lateinit var titleTextView: TextView
+    lateinit var timeTextView: TextView
+    lateinit var minPlayersTextView: TextView
+    lateinit var complexityTextView: TextView
+    lateinit var descriptionTextView: TextView
+    lateinit var posterImageView: ImageView
+    lateinit var titleTextView2: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,22 @@ class GameDetailActivity : AppCompatActivity() {
 
         titleTextView = findViewById(R.id.tvTitulo)
         titleTextView.text = game.name
+        titleTextView2 = findViewById(R.id.gameDetailToolbar)
+        //titleTextView2.setTitle(game.name)
+        //timeTextView = findViewById(R.id.tvPartida2)
+        timeTextView.text = game.time
+        minPlayersTextView = findViewById(R.id.tvJugadores2)
+        minPlayersTextView.text = "${game.minPlayers}-${game.maxPlayers}"
+        complexityTextView = findViewById(R.id.tvDificultad2)
+        complexityTextView.text = game.complexity
+        descriptionTextView = findViewById(R.id.tvDescripcion)
+        descriptionTextView.text = game.description
+        posterImageView = findViewById(R.id.gamePosterImageView)
+        Glide.with(this)
+            .load(game.posterUrl)
+            .centerCrop()
+            .into(posterImageView)
+
 
     }
 }
